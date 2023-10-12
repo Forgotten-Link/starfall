dfa14d13a477cb228f979cd5e41c8781b9421cc3
[Kerhs] <esquedaalexander@gmail.com>
2023-10-12 14:39:34 -0700

    moved files

--- scripts/Red.gd
+++ scripts/Red.gd
@@ -27,3 +27,12 @@ func _process(delta):
 	
 	position += velocity * delta
 	position = position.clamp(Vector2.ZERO, screen_size)
+	
+	if velocity.x != 0:
+		$AnimatedSprite2D.animation = "walk"
+		$AnimatedSprite2D.flip_v = false
+		# See the note below about boolean assignment.
+		$AnimatedSprite2D.flip_h = velocity.x < 0
+	elif velocity.y != 0:
+		$AnimatedSprite2D.animation = "walk"
+		
