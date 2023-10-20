b77b484c86f8d2bd5dd3e5c5e3b31d070009b03f
[Kerhs] <esquedaalexander@gmail.com>
2023-10-19 18:05:22 -0700

    stacking

--- scripts/follower.gd
+++ scripts/follower.gd
@@ -5,14 +5,20 @@ extends Area2D
 @export var default_min_distance = 50
 @export var min_distance = default_min_distance
 @export var timer = 4.0
+@export var hideTimer = 5.0
 
 func _process(delta):
 	if target:
 		# Adjust min_distance if main character has been still for 4 seconds
 		if target.time_standing_still >= timer:
 			min_distance = 0
+			if target.time_standing_still >= hideTimer:
+				hide()
+			else:
+				show()
 		else:
 			min_distance = default_min_distance
+			show()
 
 		var direction = target.global_position - global_position
 		if direction.length() > min_distance:
